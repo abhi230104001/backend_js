5d7b70fb2f3c4a172f3519e6723a8c7e4e7b407f
abhi230104001 <abhishek2304001@gmail.com>
2025-07-04 21:07:19 +0530

    file

--- src/app.js
+++ src/app.js
@@ -14,4 +14,17 @@ app.use(express.json({limit: "16kb"}))// config json to handle json data
 app.use(express.urlencoded({extended:true,limit:"16kb"}))// config express to hadle data coming from url
 app.use(express.static("public"))// this will store file images on server
 app.use(cookieParser())// config cookies
+
+// routes declaration
+ import userRouter from './routes/user.routes.js'
+
+ // routes declaration 
+
+ app.use("/api/v1/users",userRouter)
+
+ //http://localhost:8000/api/v1/users/register
+
+
+
+
 export {app}
