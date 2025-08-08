cf41c9ec05ef3be5c99cba4e295c01fd462f3dd7
abhi230104001 <abhishek2304001@gmail.com>
2025-08-08 23:38:32 +0530

    set all routes

--- src/app.js
+++ src/app.js
@@ -15,16 +15,33 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}))// config express to ha
 app.use(express.static("public"))// this will store file images on server
 app.use(cookieParser())// config cookies
 
-// routes declaration
+// routes import
  import userRouter from './routes/user.routes.js'
+ import commentRouter from "./routes/comment.routes.js";
+import likeRouter from "./routes/like.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
 
  // routes declaration 
 
- app.use("/api/v1/users",userRouter)
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/comment", commentRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/tweet", tweetRouter);
+app.use("/api/v1/video", videoRouter);
+app.use("/api/v1/healthcheck", healthcheckRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
+
 
  //http://localhost:8000/api/v1/users/register
 
 
 
 
-export {app}
+export default app;
