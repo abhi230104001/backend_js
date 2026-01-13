545bfef0110ce1c478afaf5ff9662614fcf02304
abhi230104001 <abhishek2304001@gmail.com>
2026-01-14 02:49:57 +0530

    F

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -33,16 +33,16 @@ router.route("/register").post(
 
 router.route("/login").post(loginUser)
 
-router.route("/logout").post(verifyJwt,logoutUser)
+router.route("/logout").post(verifyJWT,logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
-router.route("/change-password").post(verifyJwt, changeCurrentPassword)
-router.route("/current-user").get(verifyJwt, getCurrentUser)
-router.route("/update-account").patch(verifyJwt, updateAccountDetails)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 
-router.route("/avatar").patch(verifyJwt, upload.single("avatar"), updateUserAvatar)
-router.route("/cover-image").patch(verifyJwt, upload.single("coverImage"), updateUserCoverImage)
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
 
-router.route("/c/:username").get(verifyJwt, getUserChannelProfile) // req.paramas;
-router.route("/history").get(verifyJwt, getWatchHistory)
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile) // req.paramas;
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router
