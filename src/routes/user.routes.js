906a3d59052848dc9053823ba9c7314ecf5b0414
abhi230104001 <abhishek2304001@gmail.com>
2026-01-14 02:25:28 +0530

    fix

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -13,7 +13,7 @@ import {
     updateAccountDetails 
      } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
-import { verifyJwt } from "../middlewares/auth.middleware.js";
+import { verifyJWt } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
